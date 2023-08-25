80d4580f347946345b806e47e8f2b835b64a15d5
Luca Colagrande <luca.colagrande3@gmail.com>
2023-08-25 11:28:25 +0200

    snRuntime: Various improvements from offload study

--- target/common/test/bootrom.cc
+++ target/common/test/bootrom.cc
@@ -2,7 +2,7 @@
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 
-asm (".global tb_bootrom_start \n"
-     ".global tb_bootrom_end \n"
-     "tb_bootrom_start: .incbin \"bootrom.bin\" \n"
-     "tb_bootrom_end: \n");
+asm(".global tb_bootrom_start \n"
+    ".global tb_bootrom_end \n"
+    "tb_bootrom_start: .incbin \"bootrom.bin\" \n"
+    "tb_bootrom_end: \n");
