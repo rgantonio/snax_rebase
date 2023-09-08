510f7f9ae3ee5ae569a3b9026b816a7a328d804a
rgantonio <ryan.antonio@esat.kuleuven.be>
2023-09-08 13:29:52 +0200

    sw: Improve csr-manual-load to use internal DMA

--- target/snitch_cluster/sw/apps/csr-manual-load/src/csr-manual-load.c
+++ target/snitch_cluster/sw/apps/csr-manual-load/src/csr-manual-load.c
@@ -1,48 +1,6 @@
 #include "snrt.h"
 #include "data.h"
 
-static inline uint32_t get_array_addr(
-    uint32_t array[]
-){
-    uint32_t global_addr;
-
-    asm volatile ("la %0, %1"
-                      : "=r" (global_addr)
-                      : "i" (array)
-                      );
-
-    return global_addr;
-};
-
-static inline uint32_t lw_reg (
-    uint32_t base_address,
-    uint16_t immediate_offset
-){
-    uint32_t loaded_reg;
-
-    asm volatile(
-        "lw %[result], %[offset](%[base])"
-        : [result ]"=r" (loaded_reg)
-        : [offset] "i" (immediate_offset), [base] "r" (base_address)
-    );
-
-    return loaded_reg;
-};
-
-static inline void sw_reg(
-    uint32_t reg_data,
-    uint32_t base_address,
-    uint16_t immediate_offset
-){
-    asm volatile(
-        "sw %[reg_data], %[offset](%[base])"
-        :
-        : [reg_data] "r" (reg_data), [offset] "i" (immediate_offset), [base] "r" (base_address)
-    );
-
-    return;
-};
-
 static inline void update_csr(
     uint16_t csr_number,
     uint32_t new_value
@@ -85,58 +43,43 @@ int main() {
     uint32_t tcdm_OUT_addr;
     uint32_t final_output;
 
+    uint32_t *local_a, *local_b, *local_c, *local_o;
+
+    // Allocate space in TCDM
+    local_a   = (uint32_t *)snrt_l1_next();
+    local_b   = local_a + VEC_LEN;
+    local_c   = local_b + VEC_LEN;
+    local_o   = local_c + 1;
+
+    uint32_t dma_pre_load = snrt_mcycle();
+
+    if (snrt_is_dm_core()) {
+        size_t vector_size = VEC_LEN * sizeof(uint32_t);
+        size_t scale_size  = 1 * sizeof(uint32_t);
+        snrt_dma_start_1d(local_a, A, vector_size);
+        snrt_dma_start_1d(local_b, B, vector_size);
+        snrt_dma_start_1d(local_c, &C, scale_size);
+    }
+
+    snrt_cluster_hw_barrier();
+
     // Read the mcycle CSR (this is our way to mark/delimit a specific code region for benchmarking)
     uint32_t pre_is_compute_core = snrt_mcycle();
 
+    
     if(snrt_is_compute_core()){
 
         // This marks the start of the accelerator style of MAC operation
         uint32_t pre_load = snrt_mcycle();
 
-        // Transfer data from global memory A to local memory (tcdm)
-        // TODO: Change me to DMA transfers later
-        tcdm_addr_start = 0x10000000;
-        global_addr = get_array_addr(A);
-        tcdm_A_addr = tcdm_addr_start;
-
-        for (uint32_t i = 0; i < 20; i++){
-            working_reg = lw_reg(global_addr, i*4);
-            sw_reg(working_reg,tcdm_A_addr,i*4);
-        };
-        
-        // Transfer data from global memory B to local memory (tcdm)
-        global_addr = get_array_addr(B);
-        tcdm_B_addr = tcdm_addr_start + 80;
-
-        for (uint32_t i = 0; i < 20; i++){
-            // Just read data from vanila MAC output
-            working_reg = lw_reg(global_addr, i*4);
-            sw_reg(working_reg,tcdm_B_addr,i*4);
-        };
-
-        // Transfer single byte from global memory C to local memory (tcdm)
-        // This asm volatile extracts address of a single variable only
-        asm volatile ("la %0, %1"
-                      : "=r" (global_addr)
-                      : "i" (&C)
-                      );
-
-        tcdm_C_addr = tcdm_addr_start + 160;
-
-        working_reg = lw_reg(global_addr, 0);
-        sw_reg(working_reg,tcdm_C_addr,0);
-
-        // Get address of final output
-        tcdm_OUT_addr = tcdm_addr_start + 164;
-
         // Start of csr setup
         uint32_t csr_setup = snrt_mcycle();
 
         // Set addresses
-        update_csr(0x3d0, tcdm_A_addr);
-        update_csr(0x3d1, tcdm_B_addr);
-        update_csr(0x3d2, tcdm_C_addr);
-        update_csr(0x3d3, tcdm_OUT_addr);
+        update_csr(0x3d0, (uint32_t)local_a);
+        update_csr(0x3d1, (uint32_t)local_b);
+        update_csr(0x3d2, (uint32_t)local_c);
+        update_csr(0x3d3, (uint32_t)local_o);
         
         // Set configs
         update_csr(0x3d4, 1);   // Number of iterations
@@ -148,6 +91,7 @@ int main() {
         // Start of CSR start and poll until accelerator finishes
         uint32_t mac_csr_start = snrt_mcycle();
         uint32_t break_poll;
+
 
         while(1){
             // 0x3c3 is the CSR address for accelerator status
@@ -163,18 +107,20 @@ int main() {
         // But HWPE accelerator and snitch cores are 32-bits (word) addressable
         // If output address is divisble by 8, we read normally
         // Otherwise, we get the lower 32-bits (get the lower word address)
-        if(tcdm_OUT_addr % 8){
-            final_output = lw_reg(tcdm_OUT_addr-4, 0);
+        if(((uint32_t)local_o) % 8){
+            final_output = *(local_o-4);
         }else {
-            final_output = lw_reg(tcdm_OUT_addr, 0);
+            final_output = *local_o;
         };
 
         if(final_output != 54763){
             err = 1;
         }
         
-        uint32_t end_simulation = snrt_mcycle();
+        uint32_t end_of_computation = snrt_mcycle();
     };
+
+    snrt_cluster_hw_barrier();
 
     return err;
 }
