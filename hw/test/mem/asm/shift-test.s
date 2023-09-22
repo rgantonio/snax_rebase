ffebad3e87d4e4a9d1f612b772a47ab55e70dbdc
rgantonio <ryan.antonio@esat.kuleuven.be>
2023-09-22 11:28:02 +0200

    test: Add simple shift-test program

---
+++ hw/test/mem/asm/shift-test.s
@@ -0,0 +1,18 @@
+nop
+nop
+nop
+nop
+nop
+addi x1, x0, 0
+addi x1, x0, 15
+slli x1, x1, 8
+addi x1, x1, 72
+slli x1, x1, 8
+addi x1, x1, 122
+slli x1, x1, 8
+addi x1, x1, 7
+slli x1, x1, 8
+nop
+nop
+nop
+nop
