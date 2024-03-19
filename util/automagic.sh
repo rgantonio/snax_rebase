7be9e093cca787dcb8162b64bbf449ade6cd64e0
guilherme.paim <guilherme.paim@esat.kuleuven.be>
2024-03-19 16:45:41 +0100

    Create an automagic bashthat generate benderize and transfer data to synth

---
+++ util/automagic.sh
@@ -0,0 +1,43 @@
+# Copyright 2024 KU Leuven, Belgium
+#                INESC-ID, Portugal
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+#
+# Guilherme Paim <gppaim@ieee.org>
+# Abdelrahman Ayman Fathallah <abdelrahman.ayman@esat.kuleuven.be>
+
+#######################
+# Automagic Bash      #
+#######################
+
+#[[TODO]] - turn it a makefile
+
+TARGET_ROOT=$HOME
+PROJECT=snitch_cluster
+CFG=default
+
+# remove the flist and generated
+rm -rf snps_flist.tcl
+rm -rf target/$PROJECT/generated/*
+
+# bender checkout and bender flist
+mkdir -p target/$PROJECT/generated
+bender checkout && bender script synopsys -t synthesis -t $PROJECT > snps_flist.tcl
+python3 util/clustergen.py -c target/$PROJECT/cfg/$CFG.hjson -o target/$PROJECT/generated --wrapper
+
+#check the generation
+echo " "
+echo "--- Check the files in the local server: ---"
+echo " "
+ls -la snps_flist.tcl
+ls -la target/$PROJECT/generated/*
+echo "---"
+echo " "
+
+echo "--- Enter with your password my Lord: ---"
+echo " "
+
+#transfer
+
+ssh $USER@cygni-gw "mkdir -p $TARGET_ROOT/$PROJECT" && scp -r .bender/ hw/ target/ snps_flist.tcl $USER@cygni-gw:$TARGET_ROOT/$PROJECT && clear && echo " " && echo "Files Benderized, Generated and Transfered -- Check the Synthesis Server" && echo " " 
+
